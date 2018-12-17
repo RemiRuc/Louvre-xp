8ef93ac993b574e0a2a9ccd7bac0a1e8c4ee9c45
Remi Rucojevic <remirucojevic@MacBook-Pro-de-Remi.local>
2018-12-17 17:22:45 +0100

    UPDATE : Main menu modified (html and css)

---
+++ js/menu.js
@@ -0,0 +1,27 @@
+let Menu = {}
+
+Menu.hideMenu = function(){
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("myCanvas").style.display = "block"
+}
+
+Menu.showMenu = () => {
+    document.getElementById("menu").style.display = "flex";
+    document.getElementById("interaction").style.display = "none"
+    document.getElementById("myCanvas").style.display = "none"
+}
+
+
+
+Menu.lvlFinished = function(lvl){
+    document.querySelector(`img[data-lvl="${lvl}"]`).classList.add("active");
+    document.querySelector(`.lvl_btn[data-lvl="${lvl}"]`).classList.add("active");
+}
+
+document.querySelectorAll("#lvl_btn").forEach(el => {
+    el.addEventListener("click",()=>{
+        console.log(this.target)
+    })
+})
+
+Menu.showMenu()
