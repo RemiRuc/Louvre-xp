fadf8de8109931735e5325fd309944058b84c29b
LAGACHE Louis <llagache@g-a102a-08.local>
2018-12-18 14:47:06 +0100

    fix interaction

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ let Menu = {}
 Menu.hideMenu = function(){
     document.getElementById("menu").style.display = "none";
     document.getElementById("myCanvas").style.display = "block"
+    document.getElementById("interaction").style.display = "block"
 }
 
 Menu.showMenu = () => {
