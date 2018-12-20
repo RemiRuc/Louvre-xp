4e9e34ee5cedaa2e032f1a28e22831c0fddff3d6
RemiRuc <remi.rucojevic@gmail.com>
2018-12-21 00:11:53 +0100

    FEATURE : Sound

---
+++ js/sound.js
@@ -0,0 +1,8 @@
+let Sounds ={
+    main: new Audio("../Ressources/sound/music.mp3"),
+    porte: new Audio("../Ressources/sound/porte.mp3"),
+    mouse: new Audio("../Ressources/sound/mouse.mp3"),
+    walk: new Audio("../Ressources/sound/marche-sable.mp3"),
+}
+
+Sounds.main.loop = true
