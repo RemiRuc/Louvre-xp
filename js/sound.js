b15698592b1cfe19c967667eb8381e905319542a
LAGACHE Louis <llagache@g-a102a-08.local>
2018-12-21 13:31:21 +0100

    fin

--- js/sound.js
+++ js/sound.js
@@ -1,16 +1,15 @@
 let Sounds ={
-    main: new Audio("../Ressources/sound/music.mp3"),
-    porte: new Audio("../Ressources/sound/porte.mp3"),
-    souris: new Audio("../Ressources/sound/mouse.mp3"), /** Dont play this, we created this file directly in the Mouse Class to have 4 different audio files (otherwise, it's the same file played by 4 differents mouses and it create bugs) */
-    souris_mort: new Audio("../Ressources/sound/souris-mort.mp3"),
-    walk: new Audio("../Ressources/sound/marche-sable.mp3"),
-    transformation: new Audio("../Ressources/sound/transformation.mp3"),
-    finished: new Audio("../Ressources/sound/level-finished.mp3"),
-    back: new Audio("../Ressources/sound/back-menu.mp3"),
-    unlock: new Audio("../Ressources/sound/unlock.mp3"),
-    door: new Audio("../Ressources/sound/porte.mp3"),
+    main: new Audio("Ressources/sound/music.mp3"),
+    porte: new Audio("Ressources/sound/porte.mp3"),
+    souris: new Audio("Ressources/sound/mouse.mp3"), /** Dont play this, we created this file directly in the Mouse Class to have 4 different audio files (otherwise, it's the same file played by 4 differents mouses and it create bugs) */
+    souris_mort: new Audio("Ressources/sound/souris-mort.mp3"),
+    walk: new Audio("Ressources/sound/marche-sable.mp3"),
+    transformation: new Audio("Ressources/sound/transformation.mp3"),
+    finished: new Audio("Ressources/sound/level-finished.mp3"),
+    back: new Audio("Ressources/sound/back-menu.mp3"),
+    unlock: new Audio("Ressources/sound/unlock.mp3"),
+    door: new Audio("Ressources/sound/porte.mp3"),
 }
-
 
 Sounds.walk.volume = 0.3;
 Sounds.porte.volume = 0.3;
